b0a14f938b3945345929abf3dc41bb446d332048
Tasrul.Mostafiz <tasrulmostafiz91@gmail.com>
2021-02-23 23:55:20 +0600

    First Commit

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Todo_list from './Todo_list'
+
+class App extends Component{
+    render() {
+        
+        return (
+            <div>
+                <h1>Skyblue Again</h1>
+                <Todo_list />
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App';
+import './main.css'
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+// function component() {
+//     const element = document.createElement('div');
+  
+//     // Lodash, currently included via a script, is required for this line to work
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
